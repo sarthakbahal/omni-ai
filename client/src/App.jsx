410ac844d0bd2c082ff0dd7359c72afc748d820f
sarthakbahal <sarthakbahal.45@gmail.com>
2025-07-29 21:48:04 +0530

    Pages and routes creation.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Layout from './pages/Layout.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import WriteArticle from './pages/WriteArticle.jsx'
+import BlogTitles from './pages/BlogTitles.jsx'
+import GenerateImages from './pages/GenerateImages.jsx'
+import ReviewResume from './pages/ReviewResume.jsx'
+import RemoveBackground from './pages/RemoveBackground.jsx'
+import RemoveObject from './pages/RemoveObject.jsx'
+import Community from './pages/Community.jsx'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="community" element={<Community />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
