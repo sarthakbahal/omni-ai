ebe1c197ee683c14f70719df16d72d65601852f7
sarthakbahal <sarthakbahal.45@gmail.com>
2025-08-05 09:37:18 +0530

    Update AiTools.jsx

--- client/src/components/AiTools.jsx
+++ client/src/components/AiTools.jsx
@@ -21,7 +21,7 @@ const AiTools = () => {
             {AiToolsData.map((tool, index) => (
                 <div key={index} className='max-w-xs m-4 p-8 bg-[#FDFDFE] rounded-lg shadow-lg hover:translate-y-1 border border-gray transition-all duration-300 cursor-pointer'
                 onClick={() => user && navigate(tool.path)}>
-                    <tool.Icon className='w-12 h-12 p-3 text-white rounded-xl' style={{background: 'linear-gradient(to bottom, ${tool.bg.from}, ${tool.bg.to})'}} />
+                    <tool.Icon className='w-12 h-12 p-3 text-white rounded-xl' style={{background: `linear-gradient(to bottom, ${tool.bg.from}, ${tool.bg.to})`}} />
                     <h3 className='mt-6 mb-3 text-lg font-semibold'>{tool.title}</h3>
                     <p className='text-gray-400 text-sm max-w-[95%]'>{tool.description}</p>
                 </div>
