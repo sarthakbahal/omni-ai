2e4a2793675c0ec3c4b3c280ebbb646b9522d013
sarthakbahal <sarthakbahal.45@gmail.com>
2025-08-07 21:22:52 +0530

    Sidebar menu updated.

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -2,12 +2,15 @@ import {useState} from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { Menu, X } from 'lucide-react'
+import Sidebar from '../components/Sidebar.jsx'
+import { SignIn, useUser } from '@clerk/clerk-react'
 
 const Layout = () => {
   const navigate = useNavigate()
   const [sidebar, setSidebar] = useState(false)
+  const {user} = useUser()
 
-  return (
+  return user ? (
     <div className='flex flex-col items-start justify-start h-screen'>
       <nav className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200 '>
         <img src={assets.logo} alt="" onClick={() => navigate('/')} />
@@ -16,9 +19,19 @@ const Layout = () => {
           : <Menu onClick={() => setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden' />
         }
       </nav>
-      <Outlet />
+      <div className='flex w-full h-[calc(100vh-64px)] flex-1'>
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
+        <div className='flex-1 bg-[#F4F7FB]'> 
+          <Outlet />
+        </div>
+      </div>
+      
     </div>
      
+  ) : (
+    <div className='flex items-center justify-center h-screen'>
+      <SignIn />
+    </div>
   )
 }
 
