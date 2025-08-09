037391ce4c58a9f249eb0e8ae42df2a7f7c504c8
sarthakbahal <sarthakbahal.45@gmail.com>
2025-08-09 22:37:06 +0530

    Completed Sidebar.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useClerk, useUser } from '@clerk/clerk-react';
-import { Eraser, FileText, Hash, House, Image, Scissors, SquarePen, Users } from 'lucide-react';
+import { Protect, useClerk, useUser } from '@clerk/clerk-react';
+import { Eraser, FileText, Hash, House, Image, LogOut, Scissors, SquarePen, Users } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 
 const navItems = [
@@ -41,6 +41,22 @@ const Sidebar = ({sidebar, setSidebar}) => {
                     </NavLink>
                 ))}
             </div>
+        </div>
+
+        <div className='w-full border-t border-gray-200 p-4 px-7 flex items-center justify-between'>
+            <div className='flex items-center gap-2 cursor-pointer' onClick={openUserProfile}>
+                <img src={user.imageUrl} alt="" className='w-8 rounded-full' />
+                <div>
+                    <h1 className='text-sm font-medium'>{user.fullName}</h1>
+                    <p className='text-xs text-gray-500'>
+                        <Protect plan='premium' fallback="Free">Premium </Protect>
+                        Plan
+                    </p>
+                </div>
+            </div>
+            <LogOut onClick={signOut} className='w-4.5 text-gray-400 hover:text-gray-700
+            transition cursor-pointer' />
+
         </div>
 
     </div>
