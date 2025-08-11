58b8340b6b34b2f9152b3ea28261bef63fa75d35
sarthakbahal <sarthakbahal.45@gmail.com>
2025-08-11 10:40:14 +0530

    Completed Dashboard

--- client/src/components/CreationItem.jsx
+++ client/src/components/CreationItem.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Markdown from 'react-markdown';
 
 const CreationItem = ({item}) => {
+
+    const [expanded, setExpanded] = useState(false);
+
   return (
-    <div className='p-4 max-w-5xl text-sm bg-white border
+    <div onClick={() => setExpanded(!expanded)} className='p-4 max-w-5xl text-sm bg-white border
     border-gray-200 rounded-lg cursor-pointer'>
         <div className='flex justify-between items-center gap-4'>
             <div>
@@ -12,6 +16,29 @@ const CreationItem = ({item}) => {
             <button className='bg-[#EFF6FF] border border-[#BFD8FE]
             text-[#1E40AF] px-4 py-1 rounded-full'>{item.type}</button>
         </div>
+
+        {
+            expanded && (
+                <div>
+                    {item.type === 'image' ? (
+                        <div>
+                            <img src={item.content} alt="" className='mt-3 w-full
+                            max-w-md' />
+                        </div>
+                    ) : (
+                        <div className='mt-3 h-full overflow-y-scroll text-sm
+                        text-slate-700'>
+                        
+                            <div className='reset-tw'>
+                                <Markdown>{item.content}</Markdown>
+                            </div>
+
+                        </div>
+                    )}
+                </div>  
+            )
+        }
+
     </div>
   )
 }
