4cd2f436ce88bbcfe8e2e324e0a004d6745748ec
sarthakbahal <sarthakbahal.45@gmail.com>
2025-08-12 13:19:08 +0530

    Resume review and community page created.

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -37,7 +37,7 @@ const ReviewResume = () => {
       {}
       
       <div className='w-full max-w-lg p-4 bg-white rounded-lg flex
-      flex-col border border-gray-200 min-h-96'>
+      flex-col border border-gray-200 min-h-96 max-h-[600px]'>
         <div className='flex items-center gap-3'>
           <FileText className='w-5 h-5 text-[#00DA83]' />
           <h1 className='text-xl font-semibold'>Analysed Results</h1>
