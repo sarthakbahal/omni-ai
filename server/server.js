17f7770d25cf867c20d331449cfe31ba5c690ae7
sarthakbahal <sarthakbahal.45@gmail.com>
2025-08-15 23:01:49 +0530

    Auth middleware setup.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 
 const app = express(); // Initialize express app
 
@@ -9,7 +10,11 @@ app.use(cors()); // Enable CORS for all routes , cors is like a middleware that 
 
 app.use(express.json());  // this is used to parse incoming JSON requests and make the data available in req.body  
 
+app.use(clerkMiddleware()); //this is clerk middleware , used to get info from clerk onto the request object and into the backend
+
 app.get('/', (req,res) => res.send('Server is running')) // the parameters are the route and the calback funtion that will be executed when the route is hit, here the route is home route
+
+app.use(requireAuth()) // the home route should be accessible to anyone but after that all routes should be protected, so to do this we use the requireAuth middleware here so after this all routes will be protected
 
 const PORT = process.env.PORT || 3000; // Set the port to the value from environment variables or default to 3000
 
