c309cd7a844145272023222d4057bbe85ff71c36
sarthakbahal <sarthakbahal.45@gmail.com>
2025-09-08 02:24:12 +0530

    Update GenerateImages.jsx

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,23 +1,52 @@
 import React from 'react'
 import { useState } from 'react'
 import { Edit, Hash, Image, Sparkles } from 'lucide-react'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useAuth } from '@clerk/clerk-react'
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const GenerateImages = () => {
-  
+
   const imageStyle = ['Realistic', 'Ghibli Style', 'Anime', 'Cartoon', 'Fantasy', 'Sci-Fi', 'Portrait', '3D']
 
   const [selectedCategory, setSelectedCategory] = useState('Realistic')
   const [input, setInput] = useState('')
   const [publish, setPublish] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState(false)
+
+  const { getToken } = useAuth()
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true)
+
+      const prompt = `Generate a ${selectedCategory} image of ${input}`
+
+      const { data } = await axios.post('/api/ai/generate-image', { prompt, publish }, {
+        headers: { Authorization: `Bearer ${await getToken()}` }
+      })
+
+      if (data.success) {
+        setContent(data.content)
+      }
+      else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+
+    setLoading(false)
   }
-  
+
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start
     flex-wrap gap-4 text-slate-700'>
-      {}
+      { }
       <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg
       border border-gray-200'>
         <div className='flex items-center gap-3'>
@@ -27,20 +56,20 @@ const GenerateImages = () => {
         <p className='mt-6 text-sm font-medium'>Describe Your Image</p>
 
         <textarea onChange={(e) => setInput(e.target.value)} value={input} rows={4} className='w-full p-2 px-3 mt-2 outline-none
-        text-sm rounded-md border border-gray-300' placeholder='Describe what you want to see in the image..' required  />
+        text-sm rounded-md border border-gray-300' placeholder='Describe what you want to see in the image..' required />
 
         <p className='mt-4 text-sm font-medium'>Style</p>
         <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
           {imageStyle.map((item) => (
-            <span onClick={()=> setSelectedCategory(item)} className={`text-xs px-4 py-1 border rounded-full
+            <span onClick={() => setSelectedCategory(item)} className={`text-xs px-4 py-1 border rounded-full
             cursor-pointer ${selectedCategory === item ? 'bg-green-50 text-green-700' : 'text-gray-500 border-gray-300'}`} key={item}>{item}</span>
-          ) )}
+          ))}
         </div>
 
         <div className='my-6 flex items-center gap-2'>
           <label className='relative cursor-pointer'>
             <input type="checkbox" onChange={(e) => setPublish(e.target.checked)} checked={publish} className='sr-only peer' />
-            
+
             <div className='w-9 h-5 bg-slate-300 rounded-full
             peer-checked:bg-green-500 transition'></div>
 
@@ -52,17 +81,21 @@ const GenerateImages = () => {
 
         </div>
 
-        
-        <button className='w-full flex justify-center items-center gap-2
+
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2
         bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2
         mt-6 text-sm rounded-lg cursor-pointer'>
-          <Image className='w-5' />
+          {
+            loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span>
+              : <Image className='w-5' />
+          }
+
           Generate Image
         </button>
 
       </form>
-      {}
-      
+      { }
+
       <div className='w-full max-w-lg p-4 bg-white rounded-lg flex
       flex-col border border-gray-200 min-h-96'>
         <div className='flex items-center gap-3'>
@@ -70,14 +103,21 @@ const GenerateImages = () => {
           <h1 className='text-xl font-semibold'>Generated Image</h1>
         </div>
 
-        <div className='flex-1 flex justify-center items-center'>
-          <div className='text-sm flex flex-col items-center gap-5
+        {!content ? (
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5
           text-gray-500'>
-            <Image className='w-9 h-9' />
-            <p>Describe the image and click "Generate Image" to see the result</p>
+              <Image className='w-9 h-9' />
+              <p>Describe the image and click "Generate Image" to see the result</p>
 
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className='mt-3 h-full'>
+            <img src={content} alt="image" className='w-full h-full' />
+          </div>
+        )}
+
       </div>
     </div>
   )
