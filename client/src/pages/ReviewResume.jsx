f65ce2a4d0ce8afc068a106e2bf4955fdf965b7e
sarthakbahal <sarthakbahal.45@gmail.com>
2025-09-09 18:49:15 +0530

    Update ReviewResume.jsx

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import { useState } from 'react'
 import { FileText, Sparkles } from 'lucide-react'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useAuth } from '@clerk/clerk-react'
+import Markdown from 'react-markdown';
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const ReviewResume = () => {
-  
+
   const [input, setInput] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState(false)
+
+  const { getToken } = useAuth()
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true)
+
+      const formData = new FormData();
+      formData.append('resume', input)
+
+      const { data } = await axios.post('/api/ai/resume-review', formData,
+        { headers: { Authorization: `Bearer ${await getToken()}` } }
+      )
+
+      if (data.success) {
+        setContent(data.content)
+      }
+      else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+
+    setLoading(false)
   }
-  
+
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start
     flex-wrap gap-4 text-slate-700'>
-      {}
+      { }
       <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg
       border border-gray-200'>
         <div className='flex items-center gap-3'>
@@ -22,20 +53,21 @@ const ReviewResume = () => {
         <p className='mt-6 text-sm font-medium'>Upload Resume</p>
 
         <input onChange={(e) => setInput(e.target.files[0])} type="file" accept='application/pdf' className='w-full p-2 px-3 mt-2 outline-none
-        text-sm rounded-md border border-gray-300 text-gray-600' required  />
+        text-sm rounded-md border border-gray-300 text-gray-600' required />
 
         <p className='text-xs text-gray-500 font-light mt-1'>Supports PDF format only.</p>
 
-        <button className='w-full flex justify-center items-center gap-2
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2
         bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2
         mt-6 text-sm rounded-lg cursor-pointer'>
-          <FileText className='w-5' />
+          {loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> : <FileText className='w-5' />}
+
           Review Resume
         </button>
 
       </form>
-      {}
-      
+      { }
+
       <div className='w-full max-w-lg p-4 bg-white rounded-lg flex
       flex-col border border-gray-200 min-h-96 max-h-[600px]'>
         <div className='flex items-center gap-3'>
@@ -43,14 +75,26 @@ const ReviewResume = () => {
           <h1 className='text-xl font-semibold'>Analysed Results</h1>
         </div>
 
-        <div className='flex-1 flex justify-center items-center'>
-          <div className='text-sm flex flex-col items-center gap-5
+        {!content ? (
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5
           text-gray-500'>
-            <FileText className='w-9 h-9' />
-            <p>Upload your Resume and click "Review Resume" to see the result</p>
+              <FileText className='w-9 h-9' />
+              <p>Upload your Resume and click "Review Resume" to see the result</p>
 
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600'>
+            <div className='reset-tw'>
+              <Markdown>
+                {content}
+              </Markdown>
+            </div>
+          </div>
+        )}
+
+
       </div>
     </div>
   )
